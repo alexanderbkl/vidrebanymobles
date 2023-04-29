7d53e0258e72142f8999f6ea18f7979589f9bc9f
unknown <asederado@gmail.com>
2023-04-29 17:56:42 +0200

    add serie and model options that display rendered model

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,99 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import Born_120_Fresno from './assets/born120/Born120Fresno.jpg'
+import Born_120_Neva from './assets/born120/Born120Neva.jpg'
+import Test_120_Olmo from './assets/born120/Test120Olmo.jpg'
 import './App.css'
 
+
+const muebles = [
+  {
+    id: 0,
+    serie: 'Born 120',
+    modelos: [
+      {
+        id: 0,
+        modelo: 'Fresno',
+        img: Born_120_Fresno,
+      },
+      {
+        id: 1,
+        modelo: 'Neva',
+        img: Born_120_Neva,
+      },
+    ]
+  },
+  {
+    id: 1,
+    serie: 'Test 120',
+    modelos: [
+      {
+        id: 0,
+        modelo: 'Olmo',
+        img: Test_120_Olmo,
+      },
+    ]
+  },
+]
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [muebleId, setMuebleId] = useState(0)
+  const [muebleModelo, setMuebleModelo] = useState(0)
+
 
   return (
-    <>
+    <div className='container'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={muebles[muebleId].modelos[muebleModelo].img ? muebles[muebleId].modelos[muebleModelo].img : ''} className="img-fluid" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Moble renderitzat</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <p>Seleccionar sèrie:</p>
+        <form>
+          <select onChange={(e) => {
+            setMuebleId(parseInt(e.target.value))
+            setMuebleModelo(0)
+          }}
+            className="form-select"
+            aria-label="Default select example">
+
+            {
+              muebles.map((mueble) => {
+                return <option value={mueble.id}
+                  key={mueble.id}
+                >
+                  {mueble.serie}
+                </option>
+              })
+            }
+          </select>
+        </form>
+        <p>Seleccionar model:</p>
+        <form>
+          <select onChange={(e) => { setMuebleModelo(parseInt(e.target.value)) }} className="form-select" aria-label="Default select example">
+            {
+              muebles[muebleId].modelos.map((modelo) => {
+                return <option value={modelo.id}
+                  key={modelo.id}
+                >
+                  {modelo.modelo}
+                </option>
+              })
+            }
+
+          </select>
+        </form>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Sel·leccionar sèrie i model del <code>moble renderitzat</code>.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        © Vidre!BANY 2023
       </p>
-    </>
+
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
